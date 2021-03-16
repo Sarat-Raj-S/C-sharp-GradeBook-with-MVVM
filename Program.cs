41f98bca43212e08b3a9dd25ece3b7abf359e923
Sarat Raj <saratraj.batchu@stfc.ac.uk>
2021-03-16 12:49:41 +0530

    resolved the glitch of nor quitting when null

--- Program.cs
+++ Program.cs
@@ -6,15 +6,14 @@ namespace GradeBook
     {
         static void Main(string[] args)
          {
-            for(int i = 1; ;i++)
-            {
-                Console.WriteLine("enter name of student :");
+            while(true)
+            {   Console.WriteLine("ENTER STUDENT NAME, \n if no more sudents, enter 'null' : ");
                 string student_name = Console.ReadLine();
+                string student_name_value = student_name.ToLower();
+                if (student_name_value == "null") break;
                 Caliculation grade = new Caliculation(student_name); 
                 grade.Addgrade();
                 grade.showstatistics();
-                Console.WriteLine("if no more sudents, enter 'null'");
-                Console.ReadLine();
             }
 
         }
